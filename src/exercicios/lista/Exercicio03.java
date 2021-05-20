1d4fafe45c03bb6f36e164cd9949f7866fed2ca6
Amedeo Elmo <amedeo.elmo@mercadolivre.com>
2021-05-20 12:21:58 -0300

    Exercise 03

--- src/exercicios/lista/Exercicio03.java
+++ src/exercicios/lista/Exercicio03.java
@@ -1,7 +1,46 @@
 package exercicios.lista;
 
+import java.util.Scanner;
+
 public class Exercicio03 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Digite um número para a verificação: ");
+        int numero = scanner.nextInt();
+
+        if (isNumeroPrimo(numero))
+            System.out.println("Número é primo!");
+
+        scanner.close();
+    }
+
+    private static boolean isNumeroPrimo(final int numero) {
+        if (numero <= 1)
+            return false;
+
+        else if (numero == 2)
+            return true;
+
+        else if (numero % 2 == 0)
+            return false;
+
+        for (int i = 3; i <= Math.sqrt(numero); i += 2)
+            if (numero % i == 0)
+                return false;
+
+        return true;
+    }
+
+    // Maneira mais "custosa", só que + simples p/ leitura
+    private static boolean isNumeroPrimoOutroApproach(final int numero) {
+        if (numero <= 1)
+            return false;
+
+        for (int i = 2; i < numero; i++)
+            if (numero % i == 0)
+                return false;
+
+        return true;
     }
 }
