980d53eec59521be788c0c51acbd4eb4642fff7d
emanueli silva <emanueli.silva@mercadolivre.com>
2021-05-20 12:33:44 -0300

    Adicionando solucao exercicio 4

--- src/exercicios/lista/Exercicio04.java
+++ src/exercicios/lista/Exercicio04.java
@@ -1,7 +1,40 @@
 package exercicios.lista;
 
+import java.util.Scanner;
+
 public class Exercicio04 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Digite a quantidade de números primos desejada:");
+        int n = sc.nextInt();
+        int primos = 0;
+        int i = 0;
+
+        while (primos<n){
+            if (isNumeroPrimo(i)) {
+                primos++;
+                System.out.println(i);
+            }
+            i++;
+        }
+        sc.close();
+
+    }
+    private static boolean isNumeroPrimo(final int numero) {
+        if (numero <= 1)
+            return false;
+
+        else if (numero == 2)
+            return true;
+
+        else if (numero % 2 == 0)
+            return false;
+
+        for (int i = 3; i <= Math.sqrt(numero); i += 2)
+            if (numero % i == 0)
+                return false;
+
+        return true;
     }
 }
