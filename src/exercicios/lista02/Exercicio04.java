30dbbc59fa717ab967b35de92e1674d9c7460b3a
gsenna <guilherme.senna@mercadolivre.com>
2021-05-20 19:10:13 -0300

    Adicionando ex4 com qualquer numero de digitos

--- src/exercicios/lista02/Exercicio04.java
+++ src/exercicios/lista02/Exercicio04.java
@@ -1,17 +1,4 @@
 package exercicios.lista02;
 
 public class Exercicio04 {
-    public static void radixSort(int iArr[]) {
-
-    }
-
-    public static void main(String[] args) {
-        int iArr[] = {16223, 898, 13, 906, 235, 23, 9, 1532, 6388, 2511, 8};
-
-        radixSort(iArr);
-
-        for (int i : iArr) {
-            System.out.println(i + " ");
-        }
-    }
 }
