6ceb99dcc286e9cdc114ffbc92328bf88ce40d73
Gustavo <gustavo.wu@mercadolivre.com>
2021-05-20 12:02:37 -0300

    Adiciona resposta do Exercicio 2

--- src/exercicios/lista/Exercicio02.java
+++ src/exercicios/lista/Exercicio02.java
@@ -1,7 +1,25 @@
 package exercicios.lista;
 
+import java.util.Scanner;
+
 public class Exercicio02 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        print("Digite o primeiro numero");
+        int n = sc.nextInt();
+
+        print("Digite o numero segundo numerp");
+        int m = sc.nextInt();
+
+        for(int i=0;i<n;i++){
+            System.out.println(i*m);
+        }
+
+        sc.close();
+    }
+    
+    static void print(String text) {
+        System.out.println(text);
     }
 }
