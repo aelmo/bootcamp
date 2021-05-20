eb3928ccdfb496a89865dc2268f315c12bc0ec18
gsenna <guilherme.senna@mercadolivre.com>
2021-05-20 11:45:00 -0300

    Exercicio 1

--- src/exercicios/lista/Exercicio01.java
+++ src/exercicios/lista/Exercicio01.java
@@ -1,11 +1,14 @@
 package exercicios.lista;
 
+import java.util.Scanner;
+
 public class Exercicio01 {
     public static void main(String[] args) {
-
-    }
-
-    private boolean ePar() {
-        return false;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.println(i*2);
+        }
+        sc.close();
     }
 }
