f4bc910b98e3a2db98eec916cdf70646ba8ad137
gsenna <guilherme.senna@mercadolivre.com>
2021-05-20 15:33:33 -0300

    Adicionando Exercicio 1 lista 2

--- src/exercicios/lista02/Exercicio01.java
+++ src/exercicios/lista02/Exercicio01.java
@@ -1,4 +1,23 @@
 package exercicios.lista02;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Exercicio01 {
+    public static void main(String[] args) {
+        Integer[] array =  {52, 10, 85, 1324, 1, 13, 62, 30, 12,127};
+        Arrays.sort(array);
+        System.out.println("Ordem crescente");
+        for(Integer elem : array){
+            System.out.println(elem);
+        }
+
+        System.out.println();
+        System.out.println("Ordem decrescente");
+
+        Arrays.sort(array, Collections.reverseOrder());
+        for(Integer elem : array){
+            System.out.println(elem);
+        }
+    }
 }
